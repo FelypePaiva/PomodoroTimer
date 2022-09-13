3f811e0ca3ee238fe7f4db2bb2d26d4574b03dc6
Felype <contato@felype.dev>
2022-09-13 10:07:20 -0300

    Adiciona comportamento de clique em titulo

--- js/script.js
+++ js/script.js
@@ -4,14 +4,17 @@ var btnPause = document.getElementById("pause")
 var btnStop = document.getElementById("stop")
 var intervalo;
 var pomodoro = {
+    nome: "Pomodoro",
     tempo: [25,0],
     default: [25,0]
 }
 var pausa = {
+    nome: "Pausa",
     tempo: [5,0],
     default: [5,0]
 }
 var pausaLonga = {
+    nome: "Pausa Longa",
     tempo: [15,0],
     default: [15,0]
 }
@@ -67,6 +70,40 @@ function mostrarOpcoes(){
         listaOpcoes.style.display = "block";
     else
         listaOpcoes.style.display = "none";
+
+}
+
+var itensAcao = document.getElementsByClassName("acaoItem");
+for(let i = 0; i < itensAcao.length;i++){
+    itensAcao[i].addEventListener("click",alterarAcaoAtual)
+    //console.log(itensAcao[i].textContent);
+
+}
+function alterarAcaoAtual(event){
+    let opcaoSelecionada = event.target.textContent;
+    let acao = acaoAtual;
+    
+    switch(opcaoSelecionada){
+        case "Pomodoro":
+            acao = pomodoro;
+        break;
+        case "Pausa":
+            acao = pausa;
+        break;
+        case "Pausa Longa":
+            acao = pausaLonga;
+        break;
+        default:
+            acao = pomodoro;
+        break;
+    }
+    document.getElementById("acaoAtualNome").textContent = acao.nome;
+    //console.log("clicado em " + opcaoSelecionada);
+
+    console.table(acao);
+    acaoAtual = acao;
+    printarTempo();
+    mostrarOpcoes();
 
 }
 //#endregion
